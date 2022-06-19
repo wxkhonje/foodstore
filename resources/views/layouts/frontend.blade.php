b6056e07f7509e12fac32995d8e171c59b808a07
Xavier Khonje <xavier.khonje@nico.local>
2022-06-19 20:05:17 +0200

    Addedd a footer with Custom CSS

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -18,6 +18,7 @@
     <!-- Styles -->
     <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
 
 </head>
 <body id="page-top">
@@ -48,7 +49,7 @@
 </div>
 
 
-<!-- Footer-->
+<!-- Footer
 <footer class="bg-black text-center py-5">
     <div class="container px-5">
         <div class="text-white-50 small">
@@ -60,7 +61,156 @@
             <a href="#!">FAQ</a>
         </div>
     </div>
+</footer>-->
+
+<footer class="footer-area footer--light">
+  <div class="footer-big">
+    <!-- start .container -->
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3 col-sm-12">
+          <div class="footer-widget">
+            <div class="widget-about">
+              <img src="http://placehold.it/250x80" alt="" class="img-fluid">
+              <p>Что если тут написать другой текст на русском языке, для проверки шрифта Rubik )</p>
+              <ul class="contact-details">
+                <li>
+                  <span class="icon-earphones"></span> Call Us:
+                  <a href="tel:344-755-111">344-755-111</a>
+                </li>
+                <li>
+                  <span class="icon-envelope-open"></span>
+                  <a href="mailto:support@aazztech.com">support@aazztech.com</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <!-- Ends: .footer-widget -->
+        </div>
+        <!-- end /.col-md-4 -->
+        <div class="col-md-3 col-sm-4">
+          <div class="footer-widget">
+            <div class="footer-menu footer-menu--1">
+              <h4 class="footer-widget-title">Popular Category</h4>
+              <ul>
+                <li>
+                  <a href="#">Wordpress</a>
+                </li>
+                <li>
+                  <a href="#">Plugins</a>
+                </li>
+                <li>
+                  <a href="#">Joomla Template</a>
+                </li>
+                <li>
+                  <a href="#">Admin Template</a>
+                </li>
+                <li>
+                  <a href="#">HTML Template</a>
+                </li>
+              </ul>
+            </div>
+            <!-- end /.footer-menu -->
+          </div>
+          <!-- Ends: .footer-widget -->
+        </div>
+        <!-- end /.col-md-3 -->
+
+        <div class="col-md-3 col-sm-4">
+          <div class="footer-widget">
+            <div class="footer-menu">
+              <h4 class="footer-widget-title">Our Company</h4>
+              <ul>
+                <li>
+                  <a href="#">About Us</a>
+                </li>
+                <li>
+                  <a href="#">How It Works</a>
+                </li>
+                <li>
+                  <a href="#">Affiliates</a>
+                </li>
+                <li>
+                  <a href="#">Testimonials</a>
+                </li>
+                <li>
+                  <a href="#">Contact Us</a>
+                </li>
+                <li>
+                  <a href="#">Plan &amp; Pricing</a>
+                </li>
+                <li>
+                  <a href="#">Blog</a>
+                </li>
+              </ul>
+            </div>
+            <!-- end /.footer-menu -->
+          </div>
+          <!-- Ends: .footer-widget -->
+        </div>
+        <!-- end /.col-lg-3 -->
+
+        <div class="col-md-3 col-sm-4">
+          <div class="footer-widget">
+            <div class="footer-menu no-padding">
+              <h4 class="footer-widget-title">Help Support</h4>
+              <ul>
+                <li>
+                  <a href="#">Support Forum</a>
+                </li>
+                <li>
+                  <a href="#">Terms &amp; Conditions</a>
+                </li>
+                <li>
+                  <a href="#">Support Policy</a>
+                </li>
+                <li>
+                  <a href="#">Refund Policy</a>
+                </li>
+                <li>
+                  <a href="#">FAQs</a>
+                </li>
+                <li>
+                  <a href="#">Buyers Faq</a>
+                </li>
+                <li>
+                  <a href="#">Sellers Faq</a>
+                </li>
+              </ul>
+            </div>
+            <!-- end /.footer-menu -->
+          </div>
+          <!-- Ends: .footer-widget -->
+        </div>
+        <!-- Ends: .col-lg-3 -->
+
+      </div>
+      <!-- end /.row -->
+    </div>
+    <!-- end /.container -->
+  </div>
+  <!-- end /.footer-big -->
+
+  <div class="mini-footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="copyright-text">
+            <p>© 2018
+              <a href="#">DigiPro</a>. All rights reserved. Created by
+              <a href="#">AazzTech</a>
+            </p>
+          </div>
+
+          <div class="go_top">
+            <span class="icon-arrow-up"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </footer>
+
 <!-- Feedback Modal-->
 <div class="modal fade" id="feedbackModal" tabindex="-1" aria-labelledby="feedbackModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
